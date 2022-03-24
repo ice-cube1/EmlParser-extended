7762cf78884bf2d0540bd66977f198624d9b3a0a
cyphersprojekt <89570332+cyphersprojekt@users.noreply.github.com>
2022-03-24 14:49:36 -0300

    Update emlparser.py
    Added some comments to explain the thought process behind the script

--- emlparser.py
+++ emlparser.py
@@ -3,33 +3,44 @@ import json
 import eml_parser
 import os
 
+# I have no idea what this does. I copied it from the eml_parser pypi's site.
 def json_serial(obj):
   if isinstance(obj, datetime.datetime):
       serial = obj.isoformat()
       return serial
 
+    
+#Blank list in which we'll add our data
 adresses = []
 
+#Folder in which we have our emails
 files = os.listdir("/path/to/your/.emls/")
 
+#We iterate over every email
 for file in files:
 
     with open("/path/to/your/.emls/"+file, 'rb') as fhdl:
         raw_email = fhdl.read()
 
+    #Decode the email
     ep = eml_parser.EmlParser()
     parsed_eml = ep.decode_email_bytes(raw_email)
+    
+    #Convert the decoded email to JSON
     completejson = (json.dumps(parsed_eml, default=json_serial))
     dicc = json.loads(completejson)
 
+    #For some reason, a few of my .emls didn't seem to have a sender.
     try:
         print(dicc['header']['from'])
         adresses.append(dicc['header']['from'])
     except:
         print("\n")
 
+#We turn our list into a dictionary and then back to a list to get rid of the duplicates
 adresses = list(dict.fromkeys(adresses))
 
+#Final output
 with open("/path/to/your/output.csv", 'w') as f:
     for adress in adresses:
         f.write(str(adresses)+"\n")
